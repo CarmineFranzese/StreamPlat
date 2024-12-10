adc7f3e8c637a283d5a817d9907564aac7b9f6f7
CarmineFranzese <carmine.franzese.97@icloud.com>
2024-12-10 10:28:57 +0100

    Initial Commit

---
+++ StreamPlat/StreamPlatApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamPlatApp.swift
+//  StreamPlat
+//
+//  Created by Carmine Franzese on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StreamPlatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
