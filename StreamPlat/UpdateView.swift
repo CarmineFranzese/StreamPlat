1472a346509f2cbb5b71965501c3c9cfc1017002
CarmineFranzese <carmine.franzese.97@icloud.com>
2024-12-15 14:58:04 +0100

    saveF

---
+++ StreamPlat/UpdateView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateView.swift
+//  StreamPlat
+//
+//  Created by Carmine Franzese on 13/12/24.
+//
+
+import SwiftUI
+
+struct UpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateView()
+}
